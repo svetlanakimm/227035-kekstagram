caf3d7d1468fc5885801dd9e62564bc24f344b06
svetlanakimm <kimsvetlanau@gmail.com>
2016-09-13 21:56:07 +0300

    errors solved

--- src/pictures.js
+++ src/pictures.js
@@ -4,7 +4,7 @@
   var pictures;
 
   function JSONPRequest(url, callback) {
-    var script = document.createElement("script");
+    var script = document.createElement('script');
     script.src = url + '?callback=JSONPCallback';
     window.JSONPCallback = function(data) {
       console.log(data);
