e4902cffa53309b21868a17759b1c5b36464ea8b
svetlanakimm <kimsvetlanau@gmail.com>
2016-09-13 22:46:15 +0300

    Removed redundant logging in console

--- src/pictures.js
+++ src/pictures.js
@@ -7,8 +7,6 @@
     var script = document.createElement('script');
     script.src = url + '?callback=JSONPCallback';
     window.JSONPCallback = function(data) {
-      console.log(data);
-      console.log(pictures);
       callback(data);
     };
     document.head.appendChild(script);
