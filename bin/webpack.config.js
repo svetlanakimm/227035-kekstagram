35c2ea773eb594e3f0350f58ec24f69ac943ec54
svetlanakimm <kimsvetlanau@gmail.com>
2016-09-14 00:17:13 +0300

    template added

--- bin/webpack.config.js
+++ bin/webpack.config.js
@@ -19,6 +19,8 @@ module.exports = {
 
   entry: path.resolve(projectRoot, SRC_DIRNAME, 'main.js'),
 
+  watch: true,
+
   output: {
     filename: 'js/main.js',
     path: path.resolve(projectRoot, OUTPUT_DIRNAME),
