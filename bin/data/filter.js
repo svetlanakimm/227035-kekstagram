258b31f1da81b8e33f2d61e20999e81d3e93b5c5
Svetlana <kimsvetlanau@gmail.com>
2016-09-17 21:53:24 +0300

    filtering added

--- bin/data/filter.js
+++ bin/data/filter.js
@@ -1,5 +1,33 @@
 'use strict';
 
 module.exports = function(list, filterID) {
-  return list;
+  var filters = {
+    'filter-popular': function(list) {
+      return list;
+    },
+    'filter-new': function(list) {
+      return list
+        .filter(function(elem) {
+          return Math.abs(elem.created - new Date()) <= 3 * 24 * 60 * 60 * 1000;
+        })
+        .sort(function(a, b) {
+          return a.created - b.created;
+        });
+    },
+    'filter-discussed': function(list) {
+      return list
+        .sort(function (a,b) {
+          return b.comments - a.comments
+        });
+    }
+  };
+
+  if (filterID && (filterID in filters)) {
+    return filters[filterID](list);
+  } else {
+    return list;
+  }
+
 };
+
+
--- src/pictures.js
+++ src/pictures.js
@@ -1,7 +1,7 @@
 'use strict';
 
 define(['./load', './picture', './gallery'], function(load, Picture, gallery) {
-  var container = document.querySelector('.pictures');
+  var picturesContainer = document.querySelector('.pictures');
   var filter = document.querySelector('.filters');
   var footer = document.querySelector('footer');
   var pageNumber = 0;
@@ -13,7 +13,7 @@ define(['./load', './picture', './gallery'], function(load, Picture, gallery) {
   function renderPictures(picturesData) {
     picturesData.forEach(function(pictureData, i) {
       var picture = new Picture(pictureData, i);
-      container.appendChild(picture.element);
+      picturesContainer.appendChild(picture.element);
     });
 
     filter.classList.remove('hidden');
@@ -22,11 +22,18 @@ define(['./load', './picture', './gallery'], function(load, Picture, gallery) {
   function loadPictures(pageNumber) {
     load('/api/pictures', {
       from: pageNumber * PAGE_SIZE,
-      to: (pageNumber + 1) * PAGE_SIZE
+      to: (pageNumber + 1) * PAGE_SIZE,
+      filter: filter.querySelector(':checked').id
     }, function(data) {
       renderPictures(data);
       gallery.setPictures(data);
     })
+  }
+
+  function removePictures() {
+    while (picturesContainer.firstChild) {
+      picturesContainer.removeChild(picturesContainer.firstChild);
+    }
   }
 
   window.addEventListener('scroll', function(evt) {
@@ -34,6 +41,12 @@ define(['./load', './picture', './gallery'], function(load, Picture, gallery) {
       loadPictures(pageNumber++);
     }
   });
+
+  filter.addEventListener('change', function(evt) {
+    pageNumber = 0;
+    removePictures();
+    loadPictures(pageNumber);
+  }, true);
 
   loadPictures(0);
 });
