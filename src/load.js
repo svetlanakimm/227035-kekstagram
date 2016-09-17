32e96f83609fc0d593cdcd0faac8e78240532bda
Svetlana <kimsvetlanau@gmail.com>
2016-09-17 18:48:12 +0300

    rewite load to use xhr

--- src/load.js
+++ src/load.js
@@ -5,6 +5,18 @@
 'use strict';
 
 define(function() {
+  function load(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = function(evt) {
+      var requestObj = evt.target;
+      var response = requestObj.response;
+      var data = JSON.parse(response);
+      callback(data);
+    };
+    xhr.send();
+  }
+
   function jSONPRequest(url, callback) {
     var script = document.createElement('script');
     script.src = url + '?callback=JSONPCallback';
@@ -14,5 +26,5 @@ define(function() {
     document.head.appendChild(script);
   }
 
-  return jSONPRequest;
+  return load;
 });
